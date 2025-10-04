6d38532ca804f9dac841423315d344ea79151a37
bishwas <jha.bishwas@gmail.com>
2025-10-04 22:08:10 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python_maithili',
-    version='0.1.0',
+    version='0.2.0',
     description='Run Python code written in Maithili using Devanagari script',
     author='Bishwas Jha',
     author_email='jha.bishwas@gmail.com',
